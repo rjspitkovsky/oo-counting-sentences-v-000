5b70baee618b56ed285b5c87ac0e3f3d4d8512c7
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-30 21:36:12 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,24 +16,13 @@ class String
 
   def count_sentences
     array = self.split(/[.?!]/)
-    # count = 0
+    #count = 0
     array.select {|el| el != ""}.count
     #   if el != ""
     #     count += 1
     #   end
     # end
     # count
-    #array.count
 
-
-    # array.delete_if(" ")
-    # array.count
-
-    # array.each do |phrase|
-    #   if phrase.end_with?(".")
-    #     count += 1
-    #   end
-    # end
-    #   count
   end
 end
